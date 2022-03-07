814e5e346ab6a001c654f2839cd692a6c0881a68
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-07 08:01:59 +0700

    add: todo1 start display game

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+//TODO1-Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+//starting condition
+score0El.textContent = 0;
+score1El.textContent = 1;
+diceEl.classList.add('hidden');
