9cecdf6062029fb40f637d218a6b13c2ea5a098e
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-07 14:35:03 +0700

    add: todo4 hold current score

--- script.js
+++ script.js
@@ -23,6 +23,14 @@ let currentScore = 0;
 const scores = [0, 0];
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 //todo2
 //Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
@@ -35,20 +43,44 @@ btnRoll.addEventListener('click', function () {
 
   //3. Check for roller 1,
   if (dice !== 1) {
-
     // add dice to current score
     currentScore += dice;
     // current0El.textContent = currentScore // we will change later
 
     //Todo3: Switching active player
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
 });
+
+//todo4 hold current score
+btnHold.addEventListener('click', function () {
+  //1. add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //2. Check if player's score is >= 100
+  if (scores[activePlayer] >= 20) {
+    //finish the game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    //switch to the next player
+    switchPlayer();
+  }
+});
+
+// btnNew.addEventListener('click', function (){
+//   score0El.textContent = 0;
+//   score1El.textContent = 0;
+//   current0El.textContent = 0;
+//   current1El.textContent = 0;
+// })
